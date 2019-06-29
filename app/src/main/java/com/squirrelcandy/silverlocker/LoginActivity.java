d9e848bba8e6bffeef0351757e8bf6674517071b
SquirrelCandy <richard.c.smith85@gmail.com>
2019-06-29 13:10:47 -0400

    Initial commit

---
+++ app/src/main/java/com/squirrelcandy/silverlocker/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.squirrelcandy.silverlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //comment
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/squirrelcandy/silverlocker/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.squirrelcandy.silverlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Postponer postpone = new Postponer();
+        postpone.start();
+    }
+
+    private class Postponer extends Thread {
+        @Override
+        public void run() {
+            try {
+                sleep(2000);
+
+            }catch (InterruptedException e) {
+                //TODO handle exception
+            }
+
+            Intent lgnTnt = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(lgnTnt);
+            SplashActivity.this.finish();
+        }
+    }
+}
