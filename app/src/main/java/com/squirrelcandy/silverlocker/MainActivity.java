cd75cf6c14180e4233a5082ade84ee4cf035ddf3
SquirrelCandy <richard.c.smith85@gmail.com>
2019-06-29 14:41:03 -0400

    simple listview

--- app/src/main/java/com/squirrelcandy/silverlocker/MainActivity.java
+++ app/src/main/java/com/squirrelcandy/silverlocker/MainActivity.java
@@ -1,14 +1,42 @@
 package com.squirrelcandy.silverlocker;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    TextView tvItem;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listView);
+        tvItem =(TextView)findViewById(R.id.tvItem);
+        items = new String[] {"pikachu", "charmander", "bulbasaur", "squirtle"};
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, items);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // TODO Auto-generated method stub
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
