eb810117ae097cd47485d23b8d5ca9467b8a74d5
SquirrelCandy <richard.c.smith85@gmail.com>
2019-06-29 13:54:56 -0400

    simple admin login with attempt counter

--- app/src/main/java/com/squirrelcandy/silverlocker/LoginActivity.java
+++ app/src/main/java/com/squirrelcandy/silverlocker/LoginActivity.java
@@ -1,23 +1,46 @@
 package com.squirrelcandy.silverlocker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvAttempts;
+    private String adminPassword = "1234";
+    private int attempts = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        tvAttempts = (TextView)findViewById(R.id.tvAttempts);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //comment
+                String password = etPassword.getText().toString();
+
+                if (attempts >= 1 && adminPassword.equals(password)) {
+                    Intent tnt = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(tnt);
+                }else if (attempts > 1) {
+                    attempts--;
+                    tvAttempts.setText("Attempts Remaining: " + Integer.toString(attempts));
+                }else if (attempts == 1) {
+                    btnLogin.setEnabled(false);
+                    tvAttempts.setText("No more attempts! Try again later.");
+                }
             }
         });
     }
