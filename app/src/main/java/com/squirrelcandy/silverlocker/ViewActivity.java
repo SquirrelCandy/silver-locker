cae851cfd980da4975e9d2f52983586b727ee04a
SquirrelCandy <richard.c.smith85@gmail.com>
2019-07-02 21:57:27 -0400

    view act receives id and name of item (intent crashes)

---
+++ app/src/main/java/com/squirrelcandy/silverlocker/ViewActivity.java
@@ -0,0 +1,33 @@
+package com.squirrelcandy.silverlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+
+    TextView tvID;
+    TextView tvName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        tvID = findViewById(R.id.tvId);
+        tvName = findViewById(R.id.tvName);
+
+        int id = -1;
+        String name = "NAME";
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            id = bundle.getInt("ITEM_ID");
+            name = bundle.getString("ITEM_NAME");
+        }
+
+        tvID.setText(id);
+        tvName.setText(name);
+    }
+}
