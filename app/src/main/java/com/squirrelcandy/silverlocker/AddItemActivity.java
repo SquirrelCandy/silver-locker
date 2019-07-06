eda5e7a8e4ec5aa08b7d60425a6bc491191fb1f2
SquirrelCandy <richard.c.smith85@gmail.com>
2019-07-06 12:32:52 -0400

    declare fields

--- app/src/main/java/com/squirrelcandy/silverlocker/AddItemActivity.java
+++ app/src/main/java/com/squirrelcandy/silverlocker/AddItemActivity.java
@@ -6,13 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddItemActivity extends AppCompatActivity {
 
+    private EditText etName;
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        etName = findViewById(R.id.etName);
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
 
         Button btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
