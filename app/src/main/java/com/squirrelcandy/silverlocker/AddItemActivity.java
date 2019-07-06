e183cd2215e0fb99e17b394912de1682c9145075
SquirrelCandy <richard.c.smith85@gmail.com>
2019-07-06 12:20:22 -0400

    add button redirects to main act with list

--- app/src/main/java/com/squirrelcandy/silverlocker/AddItemActivity.java
+++ app/src/main/java/com/squirrelcandy/silverlocker/AddItemActivity.java
@@ -2,7 +2,10 @@ package com.squirrelcandy.silverlocker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class AddItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(AddItemActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
