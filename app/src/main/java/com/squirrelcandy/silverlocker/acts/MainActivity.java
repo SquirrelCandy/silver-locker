100251a5a1024efa88200d594e350be3f861f15e
SquirrelCandy <richard.c.smith85@gmail.com>
2019-07-24 20:44:39 -0400

    pulled master

--- app/src/main/java/com/squirrelcandy/silverlocker/acts/MainActivity.java
+++ app/src/main/java/com/squirrelcandy/silverlocker/acts/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,7 +12,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
